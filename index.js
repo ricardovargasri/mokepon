0e1650675dfcdb9af5fcd67e89f34b870b5fc06c
ricardo vargas <104957708+ricardovargasri@users.noreply.github.com>
2023-10-16 22:46:01 -0500

    codigo con error, seguir arreglando

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-var seleccion = document.querySelector('#seleccionar-mascota')
+var seleccion = document.querySelector('#seleccionar-mascota');
+var ataqueFuego = document.querySelector('#boton-fuego');
+var ataqueAgua = document.querySelector('#boton-agua');
+var ataqueTierra = document.querySelector('#boton-tierra');
+
+function ataqueUserEelemento(){
+    ataqueFuego.addEventListener('click', () => alert('atacas con candela'));
+    ataqueAgua.addEventListener('click', () => alert('atacas con agua')); 
+    ataqueTierra.addEventListener('click', () => alert('atacas con tierra'));
+}
+
+
+
+ataqueUserEelemento();
+/* ataquePcEelemento(); */
+
+var 
 
 seleccion.addEventListener('click', function(){
     var hipodoge = document.querySelector('#hipodoge')
+    var capipepo = document.querySelector('#capipepo')
+    var ratigueya = document.querySelector('#ratigueya')
+    var mascotaJugador = document.querySelector('#mascota-jugador')
 
     if(hipodoge.checked){
-        alert('hipodoge seleccionado')
-    }else{
+        mascotaJugador.innerHTML = 'hipodoge'
+    } else if(capipepo.checked){
+        mascotaJugador.innerHTML = 'capipepo'
+    }else if(ratigueya.checked){
+        mascotaJugador.innerHTML = 'ratigueya'
+    }
+    else{
         alert('ninguno seleccionado')
     };
+    eleccionEnemigo();
 });
+
+
+function eleccionEnemigo(){
+    let ataqueAleatorio = aleatorio(1, 3);
+    let spanMascotaEnemigo = document.querySelector('#enemiga-mascota');
+    if(ataqueAleatorio == 1){
+        spanMascotaEnemigo.innerHTML = 'hipodoge';
+    } else if(ataqueAleatorio == 2){
+        spanMascotaEnemigo.innerHTML = 'capipepo';
+    }
+    else{
+        spanMascotaEnemigo.innerHTML = 'ratigueya';
+    }
+}
+
+function aleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
